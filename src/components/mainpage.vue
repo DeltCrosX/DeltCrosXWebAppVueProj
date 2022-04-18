1a596111e231dbc83f1dd796575f4febadd87701
Boripat_Kunla <bitcoin.bkl.btc@gmail.com>
2022-04-18 19:40:04 +0700

    lockedcommit

---
+++ src/components/mainpage.vue
@@ -0,0 +1,69 @@
+<script setup>
+import Logos1 from './icons/logos1.vue';
+defineProps({
+    msg: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <WelcomeItem>
+        <template #icon>
+            <Logos1 />
+        </template>
+        <template>
+            <div class="greetings">
+                <h1 class="yellow">{{ msg }}</h1>
+                <h3>
+                    DeltCrosX Web Port
+                    <a target="__blank" href="https://dotnet.microsoft.com/en-us/languages/csharp">Microsoft C#</a>, 
+                    <a target="__blank" href="https://dotnet.microsoft.com/en-us">Microsoft DOTNET</a>, 
+                    <a target="__blank" href="https://www.iso.org/standard/74528.html">C</a>, 
+                    <a target="__blank" href="https://www.isocpp.org">C++</a>, 
+                    <a target="__blank" href="https://www.php.net">PHP</a>, 
+                    <a target="__blank" href="https://html.spec.whatwg.org">HTML</a>, 
+                    <a target="__blank" href="https://www.w3.org/Style/CSS/Overview.en.html">CSS</a>, 
+                    <a target="__blank" href="https://nodejs.org">JavaScript</a>, 
+                    <a target="__blank" href="https://www.iso.org/standard/63555.html">SQL</a>, 
+                    <a target="__blank" href="https://www.python.org">Python</a>.
+                </h3> 
+            </div>
+        </template>
+    </WelcomeItem>
+</template>
+
+<style scoped>
+
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+}
+
+h3 {
+    font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+    text-align: center;
+}
+
+@media(min-width: 1100) {
+    .greetings h1,
+    .greetings h3 {
+        text-align: center;
+    }
+}
+
+</style>
+
+<script>
+export default defineComponent({
+    setup() {
+        name: mainpageVue
+    },
+})
+</script>
---
+++ src/components/navbar.vue
@@ -0,0 +1,69 @@
+<script setup>
+
+import Nav from 'vue-nav-ui';
+
+</script>
+
+<template>
+    <Nav :navLinks="navLinks" :navConfig="navConfig" :btnConfig="btnConfig">
+        <img class="img" src="./icons/logos1.vue" alt="" srcset="" />
+    </Nav>
+</template>
+
+<script>
+export default {
+  components: {
+    Nav,
+  },
+  setup() {
+    const navLinks = ref([
+      {
+        name: "Home",
+        path: "/",
+      },
+      {
+        name: "Category",
+        path: "/category",
+      },
+      {
+        name: "Works",
+        path: "/works",
+      },
+      {
+        name: "About",
+        path: "/about",
+      },
+      {
+        name: "Contact",
+        path: "/contact",
+      },
+    ]);
+    const navConfig = ref({
+      // nav configs here
+    });
+    const btnConfig = ref({
+      // button configs here
+    });
+    return { navLinks, btnConfig, navConfig };
+  },
+};
+
+const navConfig = ref ({
+    whitespace: true,
+    navBg:"#242526",
+    navBorderRadius: "30px",
+    linkFont: "Ubuntu",
+    linkColor: "yellow",
+    responsivePosition: "top" || "bottom",
+});
+const btnConfig = ref ({
+    btnLink: true,
+    btnUrl: "",
+    btnText: "",
+    btnBg: "#12BE26",
+    btnTextColor: "white",
+    btnBorderWidth: "0",
+    btnBorderColor: "transparent",
+    btnBorderRadius: "20px",
+});
+</script>
